6a9b202a47f29846943c83b7e2156ab4f8ca7b2c
FELLIPE <fellipeguedes@icloud.com>
2024-05-30 23:14:12 -0300

    sei la acho que não mudou nada

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ print("Hello World")
 print("stop correcting me")
 idade = 29
 nome = "Fellipe"
-print("meu nome é: ",nome, "e tenho ",idade, "anos")
+print("meu nome é",nome, "e tenho",idade, "anos")
 print(idade)
 print(nome)
